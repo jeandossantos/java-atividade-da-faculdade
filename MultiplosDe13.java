18cc7026667df6d4e24215635a30e649ac17b2e9
Jean F. dos Santos <jeanddg@hotmail.com>
2024-09-14 19:55:49 -0300

    1132 - Múltiplos de 13

---
+++ MultiplosDe13.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MultiplosDe13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        if (y < x) {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        int sumNonMultiples = 0;
+
+        for (int i = x; i <= y; i++) {
+
+            if (i % 13 != 0) {
+
+                sumNonMultiples += i;
+            }
+        }
+
+        System.out.println(sumNonMultiples);
+    }
+}
